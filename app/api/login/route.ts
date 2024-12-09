5a39d421692683ecba2e5b35ae97fc04ecc0a517
Wajahat Banday <dev@wajahat.io>
2024-12-09 22:23:02 +0530

    Base Setup, Created Schema, Apis

---
+++ app/api/login/route.ts
@@ -0,0 +1,55 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import prisma from "@/utils/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { email, password } = await request.json();
+
+  // Validate input data
+  if (!email || !password) {
+    return NextResponse.json(
+      { message: "Email and Password are required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    // Find user by email
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "Invalid Email or Password" },
+        { status: 400 }
+      );
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return NextResponse.json(
+        { message: "Invalid Email or Password" },
+        { status: 400 }
+      );
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user.id, email: user.email, userName: user.userName },
+      process.env.JWT_SECRET!,
+      {}
+    );
+
+    // Return the token
+    return NextResponse.json({ message: "Login successful", token });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error logging in", error },
+      { status: 500 }
+    );
+  }
+}
---
+++ app/api/registration/route.ts
@@ -0,0 +1,53 @@
+import prisma from "@/utils/prisma";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const { userName, email, name, salutation, password } = await request.json();
+
+  // Validate input data
+  if (!userName || !email || !password || !name || !salutation) {
+    return NextResponse.json(
+      { message: "Missing required fields" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    // Hash the password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the user in the database
+    await prisma.user.create({
+      data: {
+        userName,
+        email,
+        name,
+        salutation,
+        password: hashedPassword,
+      },
+    });
+
+    // Return success response
+    return NextResponse.json(
+      { message: "User Created Successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error creating user", error },
+      { status: 500 }
+    );
+  }
+}
